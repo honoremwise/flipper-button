a132cea0055604ee0ced38ddb6291729cd468663
pohoretskyi <ci4anish@gmail.com>
2018-06-13 16:41:47 +0300

    uncomment code

--- projects/ndrop/src/lib/ndrop.component.ts
+++ projects/ndrop/src/lib/ndrop.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-// import { DragulaService } from 'ng2-dragula/ng2-dragula';
+import { DragulaService } from 'ng2-dragula/ng2-dragula';
 
 @Component({
   selector: 'N-NDrop',
@@ -25,28 +25,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NDropComponent {
 
-  // constructor(private dragulaService: DragulaService) {
-  //
-  //   // dragulaService.setOptions('first-bag', {
-  //   //   copy: true,
-  //   // });
-  //   dragulaService.drag.subscribe((value) => {
-  //     console.log(`drag: ${value[0]}`);
-  //     this.onDrag(value.slice(1));
-  //   });
-  //   dragulaService.drop.subscribe((value) => {
-  //     console.log(`drop: ${value[0]}`);
-  //     this.onDrop(value.slice(1));
-  //   });
-  //   dragulaService.over.subscribe((value) => {
-  //     console.log(`over: ${value[0]}`);
-  //     this.onOver(value.slice(1));
-  //   });
-  //   dragulaService.out.subscribe((value) => {
-  //     console.log(`out: ${value[0]}`);
-  //     this.onOut(value.slice(1));
-  //   });
-  // }
+  constructor(private dragulaService: DragulaService) {
+
+    // dragulaService.setOptions('first-bag', {
+    //   copy: true,
+    // });
+    dragulaService.drag.subscribe((value) => {
+      console.log(`drag: ${value[0]}`);
+      this.onDrag(value.slice(1));
+    });
+    dragulaService.drop.subscribe((value) => {
+      console.log(`drop: ${value[0]}`);
+      this.onDrop(value.slice(1));
+    });
+    dragulaService.over.subscribe((value) => {
+      console.log(`over: ${value[0]}`);
+      this.onOver(value.slice(1));
+    });
+    dragulaService.out.subscribe((value) => {
+      console.log(`out: ${value[0]}`);
+      this.onOut(value.slice(1));
+    });
+  }
 
   private onDrag(args) {
     let [e, el] = args;
