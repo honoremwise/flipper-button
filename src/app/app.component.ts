34d20b3d716ff485c2752a5036b735a849590e7d
Muragijimana <beastar457@gmail.com>
2018-06-14 15:16:53 +0200

    w

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { DragAndDropDataService } from "./drag-and-drop-data.service";
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
