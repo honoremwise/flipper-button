81abe02b167b64ad86aeb88b0edf031d7957d7ed
pohoretskyi <ci4anish@gmail.com>
2018-06-15 15:33:39 +0300

    Refactor code with performance optimizations

--- projects/ndrop/src/lib/ndrop-item/ndrop-item.component.ts
+++ projects/ndrop/src/lib/ndrop-item/ndrop-item.component.ts
@@ -1,48 +1,28 @@
-import {Component, OnInit, OnChanges, OnDestroy, Input, ElementRef, Output, EventEmitter} from '@angular/core';
-import {NDropComponent} from "../ndrop.component";
+import {OnInit, OnDestroy, Input, ElementRef, Output, EventEmitter, Component} from '@angular/core';
+import {NDropComponent} from '../ndrop.component';
 
 @Component({
   selector: 'n-drop-item',
-  templateUrl: './ndrop-item.component.html',
+  template: '',
   styleUrls: ['./ndrop-item.component.css']
 })
 export class NdropItemComponent implements OnInit, OnDestroy {
 
-  @Input() type: string;
   @Input() fileNameField: string;
   @Input() data: any;
   @Input() active: boolean;
-  @Input() hover: boolean;
   @Output() selectionChange = new EventEmitter<any>();
-  @Output() goToItem = new EventEmitter<any>();
 
-  constructor(public elementRef: ElementRef, private parentDropZone: NDropComponent) {
+  constructor(public elementRef: ElementRef, protected parentDropZone: NDropComponent) {
   }
 
   ngOnInit() {
-    this.elementRef.nativeElement.classList.add("n-type-" + this.type);
-    if (this.type === "folder") {
-      this.parentDropZone.registerFolder(this);
-    } else {
-      this.parentDropZone.registerFile(this);
-    }
   }
 
   ngOnDestroy() {
-    if (this.type === "folder") {
-      this.parentDropZone.unregisterFolder(this);
-    } else {
-      this.parentDropZone.unregisterFile(this);
-    }
   }
 
   public select() {
     this.selectionChange.emit(this.data);
-  }
-
-  public itemDoubleClick() {
-    if (this.type === "folder") {
-      this.goToItem.emit(this.data);
-    }
   }
 }
