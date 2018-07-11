654219eabdd2b6cac92455ad209796214f7c7bc1
respices <respinho2014@gmail.com>
2018-07-11 08:27:52 +0200

    pushing upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,26 +4,23 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {NDropModule} from 'NDrop';
-import {NChipsModule} from "NChips";
-import {NUploadModule} from 'NUpload';
 import { HttpModule, BrowserXhr } from '@angular/http';
 import { FormsModule } from '@angular/forms';
 import { ChildComponent } from './child/child.component';
 import { LoadersDirective } from './loaders.directive';
 import { HttpClientModule } from '@angular/common/http';
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+import { NUploadModule } from '../../projects/nupload/src/lib/nupload.module';
 @NgModule({
     declarations: [
         AppComponent,
         ChildComponent,
-        LoadersDirective,
-        
+        LoadersDirective
     ],
     imports: [
         HttpClientModule,
         BrowserModule,
         NDropModule,
-        NChipsModule,
         HttpModule,
         FormsModule,
         InfiniteScrollModule,
