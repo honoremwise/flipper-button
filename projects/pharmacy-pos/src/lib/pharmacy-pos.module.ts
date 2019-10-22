56b0835a9c9264b01dab9044ca15b455d194d993
gannza <respinho2014@gmail.com>
2019-10-22 20:56:31 +0200

    working for now

--- projects/pharmacy-pos/src/lib/pharmacy-pos.module.ts
+++ projects/pharmacy-pos/src/lib/pharmacy-pos.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { PharmacyPosComponent } from './pharmacy-pos.component';
-
-
-
+import { PosModule } from './pos/pos.module';
 @NgModule({
   declarations: [PharmacyPosComponent],
   imports: [
-    
+    // BrowserModule,
+    PosModule
   ],
   exports: [PharmacyPosComponent]
 })
--- projects/pharmacy-pos/src/lib/pos/pos.module.ts
+++ projects/pharmacy-pos/src/lib/pos/pos.module.ts
@@ -1,223 +1,115 @@
 import {
-  NgModule,
-  ModuleWithProviders,
-  APP_INITIALIZER,
-  ErrorHandler,
+  NgModule, ModuleWithProviders, APP_INITIALIZER, ErrorHandler,
 } from "@angular/core";
-import {
-  CommonModule
-} from "@angular/common";
-import {
-  SalePointComponent
-} from "./sale-point/sale-point.component";
-import {
-  SessionsComponent
-} from "./sessions/sessions.component";
-import {
-  PosComponent
-} from "./pos/pos.component";
-import {
-  PosRoutingModule
-} from "./pos-routing.module";
+import { CoreModule } from '../importables/core.module';
+import { Bootstrapper, init_app } from '../importables/bootstrapper.service';
+import { CurrentUser } from '../importables/auth/current-user';
+import { APP_CONFIG, DEFAULT_FLIPPER_CONFIG } from '../importables/config/flipper-config';
+import { HttpErrorHandler } from '../importables/http/errors/http-error-handler.service';
+import { BackendHttpErrorHandler } from '../importables/http/errors/backend-http-error-handler.service';
+import { Settings } from '../importables/types/models/User';
+import { HttpClientModule } from '@angular/common/http';
+import { SalePointComponent } from './sale-point/sale-point.component';
+import { SessionsComponent } from './sessions/sessions.component';
+import { UiModule } from '../importables/ui/ui.module';
+import { MaterialModule } from '../importables/material/material.module';
+import { PosRoutingModule } from './pos-routing.module';
+import { PrintOutModule } from '../importables/print-out/print-out.module';
+import { CustomersModule } from '../importables/customers/customers.module';
+import { PosProductsComponent } from './pos/load-item/pos-products/pos-products.component';
+import { ItemCategoriesComponent } from './pos/load-item/item-categories/item-categories.component';
+import { BottomSheetOverviewStock } from './pos/boottom-sheet-stock-movement/bottom-sheet-of-stock.componet';
+import { SelectCustomerModelComponent } from '../importables/customers/manage-customer/select-customer-model/select-customer-model.component';
+import { SelectCustomerTypeModalComponent } from '../importables/select-customer-type-modal/select-customer-type-modal.component';
+import { DataTableModule } from '../importables/data-table/data-table/data-table.module';
 
-import {
-  CartItemComponent,
-  CartDialog
-} from "./cart-item/cart-item.component";
-import {
-  ApiPosService
-} from "./api/api.service";
 
-import {
-  HttpClientModule
-} from "@angular/common/http";
-import {
-  PosInfiniteScrollDirective
-} from './pos/pos-infinite-scroll.directive';
-import {
-  NgxsModule
-} from '@ngxs/store';
-import {
-  ItemCategoriesComponent
-} from './pos/load-item/item-categories/item-categories.component';
-import {
-  PosProductsComponent
-} from './pos/load-item/pos-products/pos-products.component';
-import {
-  ListItemHeaderComponent
-} from './pos/list-item-header/list-item-header.component';
-import {
-  BottomSheetOverviewStock
-} from './pos/boottom-sheet-stock-movement/bottom-sheet-of-stock.componet';
-import {
-  SelectCustomerModelComponent
-} from './select-customer-model/select-customer-model.component';
-import {
-  PosSearchBarComponent
-} from './pos-search-bar/pos-search-bar.component';
-import {
-  FlexLayoutModule
-} from '@angular/flex-layout';
-import {
-  MaterialModule
-} from '../importables/material/material.module';
-import {
-  HttpModule
-} from '../importables/http/http.module';
-import {
-  PipeModuleModule
-} from '../importables/pipe-module/pipe-module.module';
-import {
-  DataTableModule
-} from '../importables/data-table/data-table/data-table.module';
-import {
-  UiModule
-} from '../importables/ui/ui.module';
-import {
-  CoreModule
-} from '../importables/core.module';
-import {
-  PrintOutModule
-} from '../importables/print-out/print-out.module';
-import {
-  CustomersModule
-} from '../importables/customers/customers.module';
-import {
-  TranslationsModule
-} from '../importables/translations/translations.module';
-import {
-  PosCateoriesState
-} from '../importables/store/states/PosCategoryStates';
-import {
-  PosStockStates
-} from '../importables/store/states/PosStockStates';
-import {
-  PosSearchStockStates
-} from '../importables/store/states/PosSearchStockStates';
-import {
-  PosOrderState
-} from '../importables/store/states/PosOrderStates';
-import {
-  Bootstrapper,
-  init_app
-} from '../importables/bootstrapper.service';
-import {
-  CurrentUser
-} from '../importables/auth/current-user';
-import {
-  APP_CONFIG,
-  DEFAULT_FLIPPER_CONFIG
-} from '../importables/config/flipper-config';
-import {
-  HttpErrorHandler
-} from '../importables/http/errors/http-error-handler.service';
-import {
-  BackendHttpErrorHandler
-} from '../importables/http/errors/backend-http-error-handler.service';
-// import {
-//   ravenErrorHandlerFactory
-// } from '../importables/errors/raven-error-handler';
-import {
-  Settings
-} from '../importables/config/settings.service';
-import {
-  SelectTaxrateModalComponent
-} from '../importables/tax-rates/select-taxrate-modal/select-taxrate-modal.component';
-import {
-  SelectCustomerTypeModalComponent
-} from '../importables/select-customer-type-modal/select-customer-type-modal.component';
-import {
-  TaxRatesComponent
-} from '../importables/tax-rates/tax-rates.component';
-import {
-  CrupdateTaxRateModalComponent
-} from '../importables/tax-rates/crupdate-tax-rate-modal/crupdate-tax-rate-modal.component';
 
 @NgModule({
   declarations: [
     SalePointComponent,
     SessionsComponent,
-    PosComponent,
-    CartItemComponent,
+    // PosComponent,
+    // CartItemComponent,
     ItemCategoriesComponent,
-    CartDialog,
+    // CartDialog,
     BottomSheetOverviewStock,
-    PosInfiniteScrollDirective,
+    // PosInfiniteScrollDirective,
     PosProductsComponent,
-    ListItemHeaderComponent,
-    SelectCustomerModelComponent,
-    PosSearchBarComponent,
-    SelectTaxrateModalComponent,
+    // ListItemHeaderComponent,
+    
+    // PosSearchBarComponent,
+    // SelectTaxrateModalComponent,
     SelectCustomerTypeModalComponent,
-    TaxRatesComponent,
-    CrupdateTaxRateModalComponent
+    // TaxRatesComponent,
+    // CrupdateTaxRateModalComponent
   ],
   imports: [
-    CommonModule,
-    HttpClientModule,
+    // CommonModule,
+     HttpClientModule,
     MaterialModule,
     PosRoutingModule,
-    PrintOutModule,
+    // PrintOutModule,
     CustomersModule,
     UiModule,
-    HttpModule,
-    PipeModuleModule,
-    FlexLayoutModule,
+    // HttpModule,
+    // PipeModuleModule,
+    // FlexLayoutModule,
     DataTableModule,
-    TranslationsModule,
-    NgxsModule.forRoot([
-      PosCateoriesState,
-      PosStockStates,
-      PosSearchStockStates,
-      PosOrderState
-    ])
+    // TranslationsModule,
+    // NgxsModule.forRoot([
+    //   PosCateoriesState,
+    //   PosStockStates,
+    //   PosSearchStockStates,
+    //   PosOrderState
+    // ])
   ],
   exports: [
-    SessionsComponent,
-    SalePointComponent,
-    PosComponent,
-    CartDialog,
-    ItemCategoriesComponent,
-    BottomSheetOverviewStock,
-    PosInfiniteScrollDirective
+    // SessionsComponent,
+    // SalePointComponent,
+    // PosComponent,
+    // CartDialog,
+    // ItemCategoriesComponent,
+    // BottomSheetOverviewStock,
+    // PosInfiniteScrollDirective
   ],
-  entryComponents: [CartDialog, BottomSheetOverviewStock, SelectTaxrateModalComponent, SelectCustomerTypeModalComponent, CrupdateTaxRateModalComponent],
-  providers: [ApiPosService],
+  // entryComponents: [CartDialog, BottomSheetOverviewStock, SelectTaxrateModalComponent, SelectCustomerTypeModalComponent, CrupdateTaxRateModalComponent],
+  // providers: [ApiPosService],
   // schemas:[CUSTOM_ELEMENTS_SCHEMA]
-  bootstrap: [PosComponent]
+  // bootstrap: [PosComponent]
 })
 
 export class PosModule {
 
-  static forRoot(): ModuleWithProviders {
-    return {
-      ngModule: CoreModule,
-      providers: [
-        Bootstrapper,
-        CurrentUser,
-        {
-          provide: APP_CONFIG,
-          useValue: DEFAULT_FLIPPER_CONFIG,
-          multi: true,
-        },
-        {
-          provide: HttpErrorHandler,
-          useClass: BackendHttpErrorHandler,
-        },
-        {
-          provide: APP_INITIALIZER,
-          useFactory: init_app,
-          deps: [Bootstrapper],
-          multi: true,
-        },
-        {
-          provide: ErrorHandler,
-          // useFactory: ravenErrorHandlerFactory,
-          deps: [Settings, CurrentUser],
-        },
-      ]
-    };
-  }
+  // static forRoot(): ModuleWithProviders {
+  //   return {
+  //     ngModule: PosModule,
+  //     providers: [
+  //       Bootstrapper,
+  //       CurrentUser,
+  //       {
+  //         provide: APP_CONFIG,
+  //         useValue: DEFAULT_FLIPPER_CONFIG,
+  //         multi: true,
+  //       },
+  //       {
+  //         provide: HttpErrorHandler,
+  //         useClass: BackendHttpErrorHandler,
+  //       },
+  //       {
+  //         provide: APP_INITIALIZER,
+  //         useFactory: init_app,
+  //         deps: [Bootstrapper],
+  //         multi: true,
+  //       },
+  //       {
+  //         provide: ErrorHandler,
+  //         // useFactory: ravenErrorHandlerFactory,
+  //         deps: [Settings, CurrentUser],
+  //       },
+  //     ]
+  //   };
+  // }
   //FIXME: when you uncomment me I error out!
   // constructor(public g: GlobalVariables) {
   // this.g.checkInternet();
